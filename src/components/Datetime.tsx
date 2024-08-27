e4be7fb9bc727f5e9791563d51449fba8b86cd1a
salomon <salomon.muriel@grupor5.com>
2024-08-27 00:43:11 -0500

    First post closer to finished draft. Small Datetime post changes.

--- src/components/Datetime.tsx
+++ src/components/Datetime.tsx
@@ -55,18 +55,9 @@ const FormattedDatetime = ({ pubDatetime, modDatetime }: DatetimesProps) => {
     month: "short",
     day: "numeric",
   });
-
-  const time = myDatetime.toLocaleTimeString(LOCALE.langTag, {
-    hour: "2-digit",
-    minute: "2-digit",
-  });
-
   return (
     <>
-      <time dateTime={myDatetime.toISOString()}>{date}</time>
-      <span aria-hidden="true"> | </span>
-      <span className="sr-only">&nbsp;at&nbsp;</span>
-      <span className="text-nowrap">{time}</span>
+      <time dateTime={myDatetime.toISOString().slice(0, 10)}>{date}</time>
     </>
   );
 };
